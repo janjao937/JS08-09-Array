1945888884b5cbc1392446a450855c07e1282045
janjao937 <janjao937@gmail.com>
2023-08-15 14:41:14 +0700

    add teacerCode On 04_03squareArr

--- 04-loop-array/03-squareArr/main.js
+++ 04-loop-array/03-squareArr/main.js
@@ -1,8 +1,33 @@
 const arr = [2, 3, 5, 7, 11];
 
-for(let i in arr)
+function SquareArr(arr)
 {
-    arr[i] **= 2;
+    const r=[];
+
+    for(let i in arr)
+    {
+        r[i] = arr[i]**2;
+    }
+    return r;
 }
 
-console.log(arr);
+console.log(SquareArr(arr));// [4, 9, 25, 49, 121]
+console.log(arr);// [2, 3, 5, 7, 11]
+
+
+
+
+
+//Teacher Code//
+//tR = teacher result
+function TeacherSquareArr(arr)
+{
+    const tR =[];
+    
+    for(let j = 0;j<=arr.length-1;j++)
+    {
+        tR.push(ar[i] **2);
+    }
+
+    return tR;
+}
